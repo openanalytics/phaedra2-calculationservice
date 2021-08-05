b895ae67826f563fb776a3522f79b98076c61158
sberberovic <sasa.berberovic@openanalytics.eu>
2021-08-05 08:48:55 +0200

    Add CalculationService class

---
+++ src/main/java/eu/openanalytics/phaedra/calculationservice/CalculationService.java
@@ -0,0 +1,12 @@
+package eu.openanalytics.phaedra.calculationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculationService.class, args);
+    }
+}
