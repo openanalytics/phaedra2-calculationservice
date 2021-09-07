5931d0ca742b8e0b4d1fa0e0a5a8e186bf1f2934
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2021-09-07 11:07:02 +0200

    Ensure tests work without RabbitMQ

--- src/test/java/eu/openanalytics/phaedra/calculationservice/IntegrationTestConfiguration.java
+++ src/test/java/eu/openanalytics/phaedra/calculationservice/IntegrationTestConfiguration.java
@@ -1,5 +1,7 @@
 package eu.openanalytics.phaedra.calculationservice;
 
+import eu.openanalytics.phaedra.scriptengine.client.ScriptEngineClient;
+import eu.openanalytics.phaedra.scriptengine.client.model.ScriptExecution;
 import org.springframework.context.annotation.Bean;
 
 import java.time.Clock;
@@ -11,6 +13,20 @@ public class IntegrationTestConfiguration {
     @Bean
     public Clock clock() {
         return Clock.fixed(Instant.parse("2042-12-31T23:59:59.00Z"), ZoneId.of("UTC"));
+    }
+
+    @Bean
+    public ScriptEngineClient scriptEngineClient() {
+        return new ScriptEngineClient() {
+            @Override
+            public ScriptExecution newScriptExecution(String targetName, String script, String input) {
+                return null;
+            }
+
+            @Override
+            public void execute(ScriptExecution scriptExecution) {
+            }
+        };
     }
 
 }
