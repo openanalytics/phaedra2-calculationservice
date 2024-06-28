743355be527405d445449a6b0f796d0320368e5c
Frederick Michielssen <fmichielssen@openanalytics.eu>
2024-06-28 17:39:13 +0200

    Fixed broken testclass

--- src/test/java/eu/openanalytics/phaedra/calculationservice/support/InMemoryResultDataServiceClient.java
+++ src/test/java/eu/openanalytics/phaedra/calculationservice/support/InMemoryResultDataServiceClient.java
@@ -144,4 +144,9 @@ public class InMemoryResultDataServiceClient implements ResultDataServiceClient 
 		return null;
 	}
 
+	@Override
+	public List<ResultFeatureStatDTO> getLatestResultFeatureStatsForPlateId(long plateId) throws ResultFeatureStatUnresolvableException {
+		return null;
+	}
+
 }
